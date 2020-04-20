5d2347abfa3857d6f0297dbcebadf04b58777782
Julien Béguin <julien.beguin99@gmail.com>
2020-04-21 01:29:51 +0200

    Test travis CI fail

--- src/test/java/ch/heigvd/gen/labo/PlayerTest.java
+++ src/test/java/ch/heigvd/gen/labo/PlayerTest.java
@@ -1,4 +1,13 @@
 package ch.heigvd.gen.labo;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class PlayerTest {
+
+    @Test
+    public void test(){
+        fail();
+    }
 }
