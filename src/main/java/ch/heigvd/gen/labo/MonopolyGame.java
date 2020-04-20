8b4289b68baf9b15d7a876e0cc61696ca7e8b0c7
Julien Béguin <julien.beguin99@gmail.com>
2020-04-21 01:18:02 +0200

    Test MonopolyGame class

--- src/main/java/ch/heigvd/gen/labo/MonopolyGame.java
+++ src/main/java/ch/heigvd/gen/labo/MonopolyGame.java
@@ -3,15 +3,19 @@ package ch.heigvd.gen.labo;
 import java.util.ArrayList;
 
 public class MonopolyGame {
-    private static final int MAX_ROUND_NB = 20;
-    private static final int MIN_PLAYER_NB = 2;
-    private static final int MAX_PLAYER_NB = 8;
-    private static final int NB_DICE = 8;
+    public static final int MAX_ROUND_NB = 20;
+    public static final int MIN_PLAYER_NB = 2;
+    public static final int MAX_PLAYER_NB = 8;
+    public static final int NB_DICE = 2;
 
     private Board board;
     private ArrayList<Die> dices;
     private ArrayList<Player> players;
 
+    /**
+     * MonopolyGame class constructor
+     * @param nbPlayer Number of player in the game
+     */
     public MonopolyGame(int nbPlayer) {
         if (nbPlayer < MIN_PLAYER_NB || nbPlayer > MAX_PLAYER_NB) {
             throw new IllegalArgumentException("Number of player must be between " + MIN_PLAYER_NB + " and " + MAX_PLAYER_NB + " (included).");
@@ -26,12 +30,18 @@ public class MonopolyGame {
         }
     }
 
+    /**
+     * Play a game of monopoly
+     */
     public void playGame() {
         for (int roundCnt = 0; roundCnt < MAX_ROUND_NB; roundCnt++) {
             playRound();
         }
     }
 
+    /**
+     * Play one round
+     */
     private void playRound() {
         for (Player player : players) {
             player.takeTurn();
