6561ab016159847416974b8c4405c613c24b652d
Julien Béguin <julien.beguin99@gmail.com>
2020-04-20 22:37:19 +0200

    Implement MonopolyGame and Player classes

--- src/main/java/ch/heigvd/gen/labo/MonopolyGame.java
+++ src/main/java/ch/heigvd/gen/labo/MonopolyGame.java
@@ -1,4 +1,41 @@
 package ch.heigvd.gen.labo;
 
+import java.util.ArrayList;
+
 public class MonopolyGame {
+    private static final int MAX_ROUND_NB = 20;
+    private static final int MIN_PLAYER_NB = 2;
+    private static final int MAX_PLAYER_NB = 8;
+    private static final int NB_DICE = 8;
+
+    private Board board;
+    private ArrayList<Die> dices;
+    private ArrayList<Player> players;
+
+    public MonopolyGame(int nbPlayer) {
+        if (nbPlayer < MIN_PLAYER_NB || nbPlayer > MAX_PLAYER_NB) {
+            throw new IllegalArgumentException("Number of player must be between " + MIN_PLAYER_NB + " and " + MAX_PLAYER_NB + " (included).");
+        }
+
+        board = new Board();
+        dices = new ArrayList<Die>(NB_DICE);
+
+        players = new ArrayList<Player>();
+        for (int i = 0; i < nbPlayer; i++) {
+            players.add(new Player("player_" + (i + 1), dices, board));
+        }
+    }
+
+    public void playGame() {
+        for (int roundCnt = 0; roundCnt < MAX_ROUND_NB; roundCnt++) {
+            playRound();
+        }
+    }
+
+    private void playRound() {
+        for (Player player : players) {
+            player.takeTurn();
+        }
+    }
+
 }
--- src/main/java/ch/heigvd/gen/labo/Player.java
+++ src/main/java/ch/heigvd/gen/labo/Player.java
@@ -1,4 +1,52 @@
 package ch.heigvd.gen.labo;
 
+import java.util.ArrayList;
+
 public class Player {
+    private String name;
+    private ArrayList<Die> dices;
+    private Board board;
+    private Piece piece;
+
+    public Player(String name, ArrayList<Die> dices, Board board) {
+        this.name = name;
+        this.dices = dices;
+        this.board = board;
+        piece = new Piece("piece_" + name, board.getFirstSquare());
+    }
+
+    /**
+     * Play player turn
+     */
+    public void takeTurn() {
+        int fvTot = 0;
+        Square oldLoc, newLoc;
+
+        // 1. Calculate a random number total between 2 and 12
+        for (Die dice : dices) {
+            dice.roll();
+            fvTot += dice.getFaceValue();
+        }
+
+        // 2. Calculate the new square location
+        oldLoc = piece.getLocation();
+        newLoc = board.getSquare(oldLoc, fvTot);
+
+        // 3. Move the player's piece from the old location to the new location
+        piece.setLocation(newLoc);
+    }
+
+    /**
+     * @return Get player name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return Get player piece
+     */
+    public Piece getPiece() {
+        return piece;
+    }
 }
