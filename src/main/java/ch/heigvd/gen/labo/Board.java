2856aef12f6529398b6ceb0d0740ae6de40b5aee
Julien Béguin <julien.beguin99@gmail.com>
2020-04-20 21:04:48 +0200

    Implement Square and Board classes

--- src/main/java/ch/heigvd/gen/labo/Board.java
+++ src/main/java/ch/heigvd/gen/labo/Board.java
@@ -1,4 +1,31 @@
 package ch.heigvd.gen.labo;
 
+import java.util.ArrayList;
+
 public class Board {
+    private static final int SQUARE_NB = 40;
+
+    private ArrayList<Square> squares;
+
+    /**
+     * Board class constructor
+     */
+    public Board() {
+        squares = new ArrayList<Square>();
+
+        squares.add(new Square("Go"));
+        for (int i = 1; i < SQUARE_NB; i++) {
+            squares.add(new Square("Square " + i));
+        }
+    }
+
+    /**
+     * Get new square by adding "moveNb" to "initialSquare"
+     * @param initialSquare Initial square
+     * @param moveNb Number of square to move
+     * @return New square
+     */
+    public Square getSquare(Square initialSquare, int moveNb) {
+        return squares.get((squares.indexOf(initialSquare) + moveNb) % squares.size());
+    }
 }
--- src/main/java/ch/heigvd/gen/labo/Square.java
+++ src/main/java/ch/heigvd/gen/labo/Square.java
@@ -1,4 +1,20 @@
 package ch.heigvd.gen.labo;
 
 public class Square {
+    private String name;
+
+    /**
+     * Square class constructor
+     * @param name Square name
+     */
+    public Square(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return Get square name
+     */
+    public String getName() {
+        return name;
+    }
 }
