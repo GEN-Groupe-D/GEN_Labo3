e8de8ebf8a7a6e01c7865b3cd804ae0799d9bf21
Julien Béguin <julien.beguin99@gmail.com>
2020-04-20 22:21:47 +0200

    Implement and test Die class

--- src/main/java/ch/heigvd/gen/labo/Die.java
+++ src/main/java/ch/heigvd/gen/labo/Die.java
@@ -1,4 +1,19 @@
 package ch.heigvd.gen.labo;
 
 public class Die {
+    private int faceValue;
+
+    /**
+     * Roll die
+     */
+    public void roll() {
+        faceValue = 1 + (int)(Math.random() * 6);
+    }
+
+    /**
+     * @return die's value
+     */
+    public int getFaceValue() {
+        return faceValue;
+    }
 }
